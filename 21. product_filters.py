d2432ce6e6bb10591c225965fa6b27f84a99ceeb
Netaji Bandi <netaji@outlook.com>
2025-04-12 15:24:31 -0500

    Product and ProductList classes with filtering functionality

---
+++ 21. product_filters.py
@@ -0,0 +1,40 @@
+class Product:
+    def __init__(self, id: int, name: str, price: float):
+        self.id = id
+        self.name = name
+        self.price = price
+
+    def __repr__(self):
+        return f"{self.id} {self.name} {self.price}"
+
+
+class ProductList:
+    def __init__(self):
+        self.products = []
+
+    def add(self, product: Product):
+        self.products.append(product)
+
+    def filter(self, condition):
+        temp = []
+        for product in self.products:
+            if condition(product):
+                temp.append(product)
+        return temp
+
+
+plist = ProductList()
+plist.add(Product(1001, "Laptop", 50000))
+plist.add(Product(1002, "Phone", 40000))
+plist.add(Product(1003, "Tablet", 10000))
+
+
+def expression(product: Product):
+    return product.price > 30000
+
+
+# f_plist = plist.filter(expression)
+# f_plist = plist.filter(lambda product: product.price > 30000)
+f_plist = plist.filter(lambda product: product.name != "Phone")
+
+print(f_plist)
